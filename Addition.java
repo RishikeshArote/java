d36b9cf41d8dab34d6d101122f9ef73ce0b776db
Rishikesh Arote <rishiarote2020@gmail.com>
2024-08-11 17:33:37 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -7,7 +7,7 @@ class Addition
     int x, y, sum;
     Scanner myObj = new Scanner(System.in);           // Create a Scanner object
     System.out.println("Type a number:");
-    x = myObj.nextInt();                              // Read user input
+    x = myObj.nextInt();                                          // Read user input
 
     System.out.println("Type another number:");
     y = myObj.nextInt();                            // Read user input
