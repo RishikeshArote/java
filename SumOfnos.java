e0b84dd00b4919920eb789e52e20a942fb6edb19
Rishikesh Arote <rishiarote2020@gmail.com>
2024-09-13 10:08:48 +0530

    sum of two nos

---
+++ SumOfnos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class SumOfnos
+{
+   public static void main(String[] args)
+     {
+       int first=10;
+       int second=20;
+       int sum=first+second;
+       System.out.println(first+ "+" +second+ "=" +sum);
+     }
+}
+----------------------------------------------------------------------------
+10+20=30
+=======================================================================================
