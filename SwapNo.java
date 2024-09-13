93a898d66b8c1f07dc1c11aed1e215e7deee622c
Rishikesh Arote <rishiarote2020@gmail.com>
2024-09-13 10:34:58 +0530

    Swap 2 Nos without using 3rd varible

---
+++ SwapNo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class SwapNo
+{
+  public static void main(String[] args)
+     {
+       int first=10;
+       int second=20;
+       first=first-second;
+       second=first+second;
+       first=second-first;
+       System.out.println("First no= " +first);
+       System.out.println("Second no= " +second);
+   }
+}
+---------------------------------------------------------------------------
+First no= 20
+Second no= 10
+=====================================================================================
