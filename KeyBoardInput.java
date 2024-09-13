c359efda0eba493efe776d69e721129ebb5b49be
Rishikesh Arote <rishiarote2020@gmail.com>
2024-09-13 10:01:57 +0530

    Add files via upload
    how to take input from keyboard.

---
+++ KeyBoardInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class KeyBoardInput
+{
+  public static void main(String[] args)
+    {
+       Scanner sc =new Scanner(System.in);
+       System.out.println("Enter a no: ");
+       int no=sc.nextInt();
+       System.out.println("You entered: "+no);
+    }
+}
+---------------------------------------------------------------------
+Enter a no:
+10
+You entered: 10
+======================================================================================
