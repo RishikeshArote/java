3eda20481c1c28300918f4531f52e102e61e2923
Rishikesh Arote <rishiarote2020@gmail.com>
2024-09-13 10:16:53 +0530

    ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
-
 public class ReverseString 
-  {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("give a string  ");
-        String str = scanner.nextLine();
-
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) 
-          {
-             reversed += str.charAt(i);
-         }
-
-        System.out.println("reversed string " + reversed);
+{
+  public static void main(String[] args) 
+    {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Give a string  ");
+      String str = scanner.nextLine();
+      String reversed = "";
+      for (int i = str.length() - 1; i >= 0; i--) 
+        {
+          reversed += str.charAt(i);
+        }
+      System.out.println("reversed string " + reversed);
     }
 }
+----------------------------------------------------------------------------
+Give a string
+Rishikesh Arote
+reversed string etorA hsekihsiR
+=====================================================================================================================================================================
